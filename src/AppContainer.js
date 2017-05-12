f422d23c62c53fc8972730aecfc8d0588080246a
Jon Woo <jw56578@gmail.com>
2017-05-12 15:51:05 -0500

    fix

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,13 +1,10 @@
 import App from "./App";
 import "./App.css";
 import { connect } from "react-redux";
-import {loadContacts} from "./actions";
 
 function mapDispatchToProps(dispatch) {
   return {
-    loadContacts: function (user) {
-      dispatch(loadContacts(user));
-    }
+
   };
 }
 
