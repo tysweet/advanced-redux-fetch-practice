92f9d2ee1ce444f6cacb00f8077f9e624a4f1cfc
Jon Woo <jw56578@gmail.com>
2017-05-12 16:03:19 -0500

    remove stuff

--- src/modules/get-one-contact.js
+++
@@ -1,9 +0,0 @@
-function getOneContact(id) {
-  const promise = fetch(`/contacts/${id}`);
-  const anotherPromise = promise.then(function (response) {
-    return response.json();
-  });
-  return anotherPromise;
-}
-
-export default getOneContact;
